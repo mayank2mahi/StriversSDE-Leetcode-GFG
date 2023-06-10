a485a81ae278380341a649faccd86de0524e9b5a
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2023-06-11 01:43:34 +0530

    optimal solution added

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,3 +1,5 @@
+// using stl(method 1)
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums)
@@ -5,3 +7,40 @@ public:
         next_permutation(nums.begin(),nums.end());
     }
 };
+
+// method 2
+
+vector<int> nextGreaterPermutation(vector<int> &A) {
+    
+    int n = A.size(); // size of the array.
+    // Step 1: Find the break point:
+    int ind = -1; // break point
+    for (int i = n - 2; i >= 0; i--) {
+        if (A[i] < A[i + 1]) {
+            // index i is the break point
+            ind = i;
+            break;
+        }
+    }
+
+    // If break point does not exist:
+    if (ind == -1) {
+        // reverse the whole array:
+        reverse(A.begin(), A.end());
+        return A;
+    }
+
+    // Step 2: Find the next greater element
+    //         and swap it with arr[ind]:
+
+    for (int i = n - 1; i > ind; i--) {
+        if (A[i] > A[ind]) {
+            swap(A[i], A[ind]);
+            break;
+        }
+    }
+
+    // Step 3: reverse the right half:
+    reverse(A.begin() + ind + 1, A.end());
+    return A;
+}
