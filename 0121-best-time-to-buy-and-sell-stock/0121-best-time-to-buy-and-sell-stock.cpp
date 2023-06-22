e3f4ecfdbcce533863103c8e0e0ce7588dcf74b4
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2023-06-22 15:31:18 +0530

    Update 0121-best-time-to-buy-and-sell-stock.cpp

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,9 +1,9 @@
+//Optimal Solution
 class Solution
 {
 public:
     int maxProfit(vector<int> &prices)
     {
-
         int c = INT_MAX, pr = 0;
         for (int i = 0; i < prices.size(); i++)
         {
@@ -12,4 +12,22 @@ public:
         }
         return pr;
     }
+};
+
+//Brute Force
+class Solution
+{
+public:
+   int maxProfit(vector<int> &prices) 
+   {
+    int pr = 0;
+    for (int i = 0; i < prices.size(); i++) {
+        for (int j = i + 1; j < prices.size(); j++) {
+            if (prices[j] > prices[i]) {
+            pr = max(prices[j] - prices[i], pr);
+            }
+        }
+     }
+    return pr;
+  }
 };
