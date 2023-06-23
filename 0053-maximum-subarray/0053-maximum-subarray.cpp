0aed2dbe111c99ab76e03e91be7d1cb1b6ece925
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2023-06-23 22:48:17 +0530

    Update 0053-maximum-subarray.cpp

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,3 +1,27 @@
+//Brute Force
+
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int maxi = INT_MIN;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int sum = 0;
+            for (int j = i; j < nums.size(); j++)
+            {
+                sum = sum + nums[j];
+                maxi = max(maxi, sum);
+            }
+        }
+        return maxi;
+    }
+};
+
+
+//Optimal solution
+
 class Solution
 {
 public:
