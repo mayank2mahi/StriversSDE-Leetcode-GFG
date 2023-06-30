adbf52eb3235d5bba11c0ae02a8e7fb144c67163
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2023-06-30 23:03:23 +0530

    Added optimal solution

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,34 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int i = 0, j = 0;
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = 0; j < nums.size(); j++){
-                if((nums[i] + nums[j] == target) && (i != j)){
-                    return {i, j};
-                }
-            }
+// Brute Force
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int i = 0, j = 0;
+//         for(int i = 0; i < nums.size(); i++){
+//             for(int j = 0; j < nums.size(); j++){
+//                 if((nums[i] + nums[j] == target) && (i != j)){
+//                     return {i, j};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
+
+// Optimal Solution
+
+#include <bits/stdc++.h>
+using namespace std;
+string twoSum(int n, vector<int> &arr, int target) {
+    sort(arr.begin(), arr.end());
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == target) {
+            return "YES";
         }
-        return {};
+        else if (sum < target) left++;
+        else right--;
     }
-};
+    return "NO";
+}
