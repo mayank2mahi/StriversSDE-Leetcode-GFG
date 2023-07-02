e854a5557693e84831741a06b2306e82f41cf02a
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2023-07-02 21:36:10 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,22 +1,40 @@
 // Brute Force
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i = 0, j = 0;
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 0; j < nums.size(); j++){
+                if((nums[i] + nums[j] == target) && (i != j)){
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
 
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         int i = 0, j = 0;
-//         for(int i = 0; i < nums.size(); i++){
-//             for(int j = 0; j < nums.size(); j++){
-//                 if((nums[i] + nums[j] == target) && (i != j)){
-//                     return {i, j};
-//                 }
-//             }
-//         }
-//         return {};
-//     }
-// };
+
+// Better Solution (HASH MAPPING)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mpp;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            int num = nums[i];
+            int moreNeed = target - num;
+            if(mpp.find(moreNeed) != mpp.end()){
+                return {mpp[moreNeed] , i};
+            }
+            mpp[num] = i;
+        }
+        return {-1, -1};
+    }
+};
+
 
 // Optimal Solution (TWO POINTER APPROACH)
-
 #include <bits/stdc++.h>
 using namespace std;
 string twoSum(int n, vector<int> &arr, int target) {
