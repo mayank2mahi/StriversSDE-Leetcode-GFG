53622ec288adef498cf023dea768c416148639c5
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2023-08-30 11:56:48 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (7.37%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& v) {
+        int n = v.size();
+        for(int i = n-1; i > -1; i--){
+            if(i == n-1){
+                v[i]++;}
+            if(v[i]==10){
+                v[i]=0;
+                if(i != 0){
+                    v[i-1]++;}
+                else{
+                    v.push_back(0);
+                    v[i]=1;}
+            }
+        }
+        return v;
+    }
+};
