bb79b79ac0d0804d2df3848f76a6b3fdd1a8ac83
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2024-05-30 22:30:16 +0530

    Time: 31 ms (41.07%), Space: 24.9 MB (35.95%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,32 +1,19 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> result;
-        vector<vector<int>> charCount;
         
-        for (const auto& str : strs) {
-            vector<int> count(26, 0);
-            for (char c : str)
-                count[c - 'a']++;
-            
-            bool found = false;
-            for (int i = 0; i < result.size(); ++i) {
-                if (charCount[i] == count) {
-                    result[i].push_back(str);
-                    found = true;
-                    break;
-                }
-            }
-            
-            if (!found) {
-                result.push_back({str});
-                charCount.push_back(count);
-            }
+        vector<vector<string>> ans;
+        
+        unordered_map<string, vector<string>> mpp;
+        for(int i = 0; i < strs.size(); i++){
+            string tt = strs[i];
+            sort(tt.begin(),tt.end());
+            mpp[tt].push_back(strs[i]);
         }
         
-        return result;
+        for(auto it : mpp){
+            ans.push_back(it.second);
+        }
+        return ans;
     }
 };
