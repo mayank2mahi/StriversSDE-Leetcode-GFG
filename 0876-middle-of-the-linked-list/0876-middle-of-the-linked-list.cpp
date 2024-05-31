f01024b336bd06c986d5af38f56e3a9be127ca0a
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2024-05-31 23:40:55 +0530

    Time: 4 ms (34.85%), Space: 8.7 MB (9.79%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,8 +11,8 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        // using tortoise method
-        ListNode *slow = head, *fast = head;
+        ListNode* slow = head;
+        ListNode* fast = head;
         while(fast != NULL && fast->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
