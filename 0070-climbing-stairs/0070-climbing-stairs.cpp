0f7fcc350d16dbfd681177266fac4c2646ec6d43
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2024-06-14 01:24:50 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (51.00%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> memo(n + 1, -1);
+        return solve(n, memo);
+    }
+
+private:
+    int solve(int n, vector<int>& memo) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        memo[n] = solve(n - 1, memo) + solve(n - 2, memo);
+        return memo[n];
+    }
+};
