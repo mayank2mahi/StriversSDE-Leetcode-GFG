4d9802ee1581b7affb9b601e92f3ebc8b80387d1
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2024-07-29 20:52:34 +0530

    Time: 23 ms (55.75%), Space: 26.8 MB (36.28%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,48 @@
+// WHEN THERE IS NO CIRCULAR LOOP
+
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+//         vector<int> v(nums.size(), -1);
+//         stack<int> st;
+        
+//         for (int i = 2 * nums.size() - 1; i >= 0; i--) {
+//             while (!st.empty() && st.top() <= nums[i]) {
+//                 st.pop();
+//             }
+//             if (st.empty()) {
+//                 v[i] = -1;
+//             }
+//             else{
+//                 v[i] = st.top();
+//             }
+//             st.push(nums[i]);
+//         }
+//         return v;
+//     }
+// };
+
+
+// WHEN CIRCULAR LOOP IS INVOLVED
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int> v(nums.size(), -1);
+        stack<int> st;
+        
+        for (int i = 2 * nums.size() - 1; i >= 0; i--) {
+            int k = i % nums.size();
+            while (!st.empty() && st.top() <= nums[k]) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                v[k] = st.top();
+            }
+            st.push(nums[k]);
+        }
+        
+        return v;
+    }
+};
+
