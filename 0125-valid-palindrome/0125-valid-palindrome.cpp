345ae1487b3af3307affaddf62bd2e98ca830ceb
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2024-07-31 22:40:34 +0530

    Time: 9 ms (21.00%), Space: 9.5 MB (7.47%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string t;
+        string v;
         for(int i = 0; i < s.size(); i++){
-            if(isalnum(s[i])) {
-                t.push_back(tolower(s[i]));
+            if(isalnum(s[i])){
+                v.push_back(tolower(s[i]));
             }
         }
-        string reversed_t = t; // Make a copy of t
-        reverse(reversed_t.begin(), reversed_t.end()); // Reverse the copy
-        
-        return t == reversed_t; // Check if t is equal to its reverse
+        string f = v;
+	    reverse(f.begin(), f.end());
+        return f == v;
     }
 };
