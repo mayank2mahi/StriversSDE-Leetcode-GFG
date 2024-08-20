658364ff74833d3f6f5b72bac321a35cf4099b32
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2024-08-20 16:14:52 +0530

    Time: 59 ms (84.95%), Space: 45.4 MB (58.43%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,16 +1,37 @@
-class Solution {
+// class Solution {                            // Brute Force Approach
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+//         int c = 0;
+//         for(int i = 0; i < nums.size(); i++){
+//             int sum = 0;
+//             for(int j = i; j < nums.size(); j++){
+//                 sum = sum + nums[j];
+//                 if(sum == k){
+//                     c++;
+//                 }
+//             }
+//         }
+//         return c;
+//     }
+// };
+
+class Solution {                            
 public:
     int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        
+        int csum = 0;
         int c = 0;
+        mp.insert({0, 1});  
         for(int i = 0; i < nums.size(); i++){
-            int sum = 0;
-            for(int j = i; j < nums.size(); j++){
-                sum = sum + nums[j];
-                if(sum == k){
-                    c++;
-                }
+            csum = csum + nums[i];
+            int target = csum - k;
+            if(mp.find(target) != mp.end()){
+                c = c + mp[target];
             }
+            mp[csum]++;
         }
         return c;
     }
 };
+
