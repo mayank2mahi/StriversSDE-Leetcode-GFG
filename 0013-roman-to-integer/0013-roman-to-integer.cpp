a2b63949b738085cdabb26e920602498bbc44ccb
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2024-08-30 11:58:45 +0530

    Time: 8 ms (69.30%), Space: 13.3 MB (41.89%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,49 +1,28 @@
 class Solution {
 public:
-    int romanToInt(string str) {
-        int n = str.length();
-        if(n == 0){
-            return 0;
-        }
-        int res = 0;
-        for(int i = 0; i < n; i++){
-            switch(str[i]){
-                case 'I':
-                    res += 1;
+    int romanToInt(string s) {
+        vector<pair<char, int>> pp = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500},                 {'M', 1000}
+        };
+        vector<int> v;
+        
+        for(int i = 0; i < s.size(); i++){
+            for(auto h : pp){
+                if(s[i] == h.first){
+                    v.push_back(h.second);
                     break;
-                case 'V':
-                    res += 5;
-                    break;
-                case 'X':
-                    res += 10;
-                    break;
-                case 'L':
-                    res += 50;
-                    break;
-                case 'C':
-                    res += 100;
-                    break;
-                case 'D':
-                    res += 500;
-                    break;
-                case 'M':
-                    res += 1000;
-                    break;
+                }
             }
         }
-        for (int i = 1; i < n; i++) {
-            if ((str[i] == 'V' || str[i] == 'X') && str[i - 1] == 'I') {
-                res -= 1 + 1;
-            }
 
-            if ((str[i] == 'L' || str[i] == 'C') && str[i - 1] == 'X') {
-                res -= 10 + 10;
-            }
-
-            if ((str[i] == 'D' || str[i] == 'M') && str[i - 1] == 'C') {
-                res -= 100 + 100;
+        int sum = 0;
+        for(int i = 0; i < v.size(); i++){
+            if(i < v.size() - 1 && v[i] < v[i + 1]){
+                sum -= v[i];
+            } else {
+                sum += v[i];
             }
         }
-        return res;
+        return sum;
     }
 };
