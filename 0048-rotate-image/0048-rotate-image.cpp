22290ebf60e9279aa0faaa4fcd22101209fc1345
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2024-09-30 16:30:14 +0530

    Time: 0 ms (100.00%), Space: 9 MB (58.72%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-    for (int i = 0; i < matrix.size(); i++) {
-    for (int j = 0; j < i; j++) {
-        swap(matrix[i][j], matrix[j][i]);
+        int n = matrix.size();
+        int m = n;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < m; j++){                  // transpose
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i = 0; i < n; i++){                      // reverse rows
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
-    for (int i = 0; i < matrix.size(); i++) {
-    reverse(matrix[i].begin(), matrix[i].end());
-    }
-  }
 };
-
-//By observation, we see that the first column of the original matrix is the reverse of the first row of the rotated matrix, so that’s why we transpose the matrix and then reverse each row
