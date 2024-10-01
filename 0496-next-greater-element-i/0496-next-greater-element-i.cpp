7ecd8a9e503bfc9e0747e063cde361f9523cc180
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2024-10-01 18:33:44 +0530

    Time: 7 ms (39.69%), Space: 11.1 MB (77.10%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,29 +1,27 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> mp;
-        stack<int> st;
-        
-        for(int i = nums2.size() - 1; i >= 0; i--){
-            while(!st.empty() && st.top() <= nums2[i]){
-                st.pop();
-            }
-            
-            int res = 0;
-            if(st.empty()){
-                res = -1;
-            }
-            else{
-                res = st.top();
-            }
-            mp.insert({nums2[i], res});
-            st.push(nums2[i]);
-            
+        vector<int> v;
+        for (int i = 0; i < nums1.size(); i++) {
+            v.push_back(dj(nums1[i], nums2));
         }
-        vector<int> ans;
-        for(auto j : nums1){
-            ans.push_back(mp[j]);
+        return v;
+    }
+    
+    int dj(int j, const vector<int>& nums2) {
+        for (int i = 0; i < nums2.size(); i++) {
+            if (nums2[i] == j) {
+                for (int k = i + 1; k < nums2.size(); k++) {
+                    if (nums2[k] > j) {
+                        return nums2[k];
+                    }
+                }
+                break;
+            }
         }
-        return ans;
+        return -1;
     }
 };
