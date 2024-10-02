eefae6ab40e8ce632db1d093903fba75e38216e6
Mayank Singh <122252694+mayank2mahi@users.noreply.github.com>
2024-10-02 16:16:50 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (63.51%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        if (word.size() == 1) {
+        int c = 0;
+        for(int i = 0; i < word.size(); i++){
+            if(isupper(word[i])){
+                c++;
+            }
+        }
+        if(c == word.size() || c == 0){
             return true;
         }
-        bool isFirstUpper = isupper(word[0]);
-        bool allLowerRest = true, allUpperRest = true;
-        
-        for (int i = 1; i < word.size(); i++) {
-            if (islower(word[i])) {
-                allUpperRest = false;
-            }
-            if (isupper(word[i])) {
-                allLowerRest = false;
-            }
+        if(c == 1 && isupper(word[0])){
+            return true;
         }
-        if (isFirstUpper) {
-            return allLowerRest || allUpperRest;
-        } else {
-            return allLowerRest;
+        else{
+            return false;
         }
     }
 };
